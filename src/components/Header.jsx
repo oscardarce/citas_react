13361014ba869d2acfd29bdb40ebdb3576b6c203
Oscar Darce Reyes <oscardarce@gmail.com>
2023-05-10 00:01:57 -0600

    Se modifico solo el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto"> Seguimiento Paciente {" "}
+            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto"> Seguimiento Paciente {" "} con GitHub {" "}
                 <span className="text-indigo-600"> Veterinaria</span>
             </h1>
     )
